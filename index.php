b15a2d4776fae185e211fa2e94f7469625e2d0e3
Michael Dela Cuadra <invisoagent4@gmail.com>
2024-10-31 14:29:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// index.php
+require 'config.php';
+
+$query = $pdo->query("SELECT * FROM users");
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Management</title>
+</head>
+<body>
+    <h1>User List</h1>
+    <a href="create.php">Add New User</a>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?= htmlspecialchars($user['id']) ?></td>
+                <td><?= htmlspecialchars($user['name']) ?></td>
+                <td><?= htmlspecialchars($user['email']) ?></td>
+                <td><?= htmlspecialchars($user['age']) ?></td>
+                <td>
+                    <a href="update.php?id=<?= $user['id'] ?>">Edit</a>
+                    <a href="delete.php?id=<?= $user['id'] ?>" onclick="return confirm('Are you sure?')">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
